41f55163d82ac67f62208c821e18e9c59ab6c2cd
Hannamari Heiniluoma <hannamari.h.heiniluoma@jyu.fi>
2022-01-18 13:48:31 +0200

    Add biblio title in BiblioList before link

--- src/components/BiblioList.js
+++ src/components/BiblioList.js
@@ -13,14 +13,16 @@ const BiblioList = ({ kohaSuccess, biblionumbers, convertedTitles, checked }) =>
     if (kohaSuccess) {
         return (
             <div>
-                <p>Seuraavat tietueet tallennettu Kohaan</p>
-                <ListGroup>
+                <p>Seuraavat tietueet tallennettu Kohaan:</p>
+                <ListGroup className="pb-5">
                     {
-                        biblionumbers.map(biblionumber => {
+                        biblionumbers.map((biblionumber, index) => {
+
+                            const title = postedTitles[index]
 
                             return (
                                 <ListGroup.Item key={biblionumber}>
-                                    Linkki Kohaan <a href={`https://app1.jyu.koha.csc.fi/cgi-bin/koha/catalogue/detail.pl?biblionumber=${biblionumber}`} target="_blank" rel="noreferrer">{`https://app1.jyu.koha.csc.fi/cgi-bin/koha/catalogue/detail.pl?biblionumber=${biblionumber}`}</a>
+                                    {title} <a href={`https://app1.jyu.koha.csc.fi/cgi-bin/koha/catalogue/detail.pl?biblionumber=${biblionumber}`} target="_blank" rel="noreferrer">{`https://app1.jyu.koha.csc.fi/cgi-bin/koha/catalogue/detail.pl?biblionumber=${biblionumber}`}</a>
                                 </ListGroup.Item>)
                         })
                     }
